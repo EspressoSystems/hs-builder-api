716e2ba5674750a9c3f1fb6ec3f0994461a82a52
QuentinI <QuentinI@users.noreply.github.com>
2024-02-20 17:37:08 +0000

    deploy: 071619ba71d0f9c108a0663548528677c843ecd7

---
+++ trait.impl/tide_disco/error/trait.Error.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"hs_builder_api":[["impl Error for <a class=\"enum\" href=\"hs_builder_api/builder/enum.Error.html\" title=\"enum hs_builder_api::builder::Error\">Error</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
