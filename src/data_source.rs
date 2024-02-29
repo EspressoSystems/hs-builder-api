e485bb924332e8e3223e28b98ba981f49fc8f777
Nathan F Yospe <nyospe@gmail.com>
2024-02-29 12:47:36 -0500

    Fixing a rogue dependency specification

--- src/data_source.rs
+++ src/data_source.rs
@@ -1,8 +1,8 @@
 use async_trait::async_trait;
 use hotshot_types::{
-    data::VidCommitment,
     traits::{node_implementation::NodeType, signature_key::SignatureKey},
     utils::BuilderCommitment,
+    vid::VidCommitment,
 };
 use tagged_base64::TaggedBase64;
 
